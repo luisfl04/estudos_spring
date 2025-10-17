9e47a4cbf02146a359836fc9711f29b5750672f5
Luis Felipe <luisfelipecontato08@gmail.com>
2025-10-17 08:56:40 -0300

    feat: Adiciona exemplo de uma aplicação spring funcional

---
+++ desvendando_spring/src/main/java/est/desvendando_spring/DesvendandoSpringApplication.java
@@ -0,0 +1,13 @@
+package est.desvendando_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesvendandoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesvendandoSpringApplication.class, args);
+	}
+
+}
---
+++ desvendando_spring/src/test/java/est/desvendando_spring/DesvendandoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package est.desvendando_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesvendandoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
